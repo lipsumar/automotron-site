a8c77cb4db65709e1836ada10faf41e56db7f9b0
Emmanuel Pire <piremmanuel@gmail.com>
2020-12-09 00:43:20 +0100

    firsst setup

---
+++ pages/doc/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>doc</title>
+      </Head>
+      <h1>DOC!</h1>
+    </div>
+  );
+}
